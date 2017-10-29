8c2268e0f43f5ec9493098eabe25166ff07a1157
VincentLinsanity <x123356@gmail.com>
2017-10-29 14:57:42 +0800

    add gracefulShutdown in server.js

--- server.js
+++ server.js
@@ -38,6 +38,28 @@ process.on("uncaughtException", function(err) {
   console.error(err); // handle the error safely
 });
 
+// this function is called when you want the server to die gracefully
+// i.e. wait for existing connections
+var gracefulShutdown = function() {
+  console.log("Received kill signal, shutting down gracefully.");
+  server.close(function() {
+    console.log("Closed out remaining connections.");
+    process.exit()
+  });
+  
+   // if after 
+   setTimeout(function() {
+       console.error("Could not close connections in time, forcefully shutting down");
+       process.exit()
+  }, 10*1000);
+}
+
+// listen for TERM signal .e.g. kill 
+process.on ('SIGTERM', gracefulShutdown);
+
+// listen for INT signal e.g. Ctrl-C
+process.on ('SIGINT', gracefulShutdown);  
+
 server.listen(config.api.port, function() {
   console.log(`Api server listening on port: ${config.api.port}`);
 });
